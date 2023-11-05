62132553a33a4beeea1ffb658a10d9d7bd1e6331
Lob77 <uueel1224@gmail.com>
2023-11-05 16:46:13 +0900

    [Add] GET,POST,PUT,DELETE API 예시 코드 추가

--- src/main/java/com/springboot/api/controller/HelloController.java
+++ src/main/java/com/springboot/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.springboot.hello.controller;
+package com.springboot.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
