14325e168f8ad08f65c55e7169db6ce026b0a020
Lob77 <uueel1224@gmail.com>
2023-11-19 02:13:23 +0900

    [데이터베이스 연동, api  테스트]

---
+++ src/main/java/com/springboot/jpa/data/dto/ChangeProductName.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangeProductName {
+
+    private Long number;
+
+    private String name;
+}
